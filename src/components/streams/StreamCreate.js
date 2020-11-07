4e823219cc9bf9916b474e99c0cc028c76404edf
kishan-1234 <kishan.ngm@gmail.com>
2020-11-07 23:54:50 +0530

    Changes upto form creation middle

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,9 +1,31 @@
  import React from 'react';
+ import { Field, reduxForm } from 'redux-form';
 
- const StreamCreate = () => {
-     return <div>
-         StreamCreate
-     </div> ;
+ class StreamCreate extends React.Component {
+
+    renderInput({input, label} ) {
+        return (
+         <div className="field">
+             <label >{label}</label>
+             <input  {...input} />
+        </div>
+        );
+     }
+
+     onSubmit(event) {
+         event.preventDefault();
+     }
+
+     render() {
+         return (
+             <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+                 <Field name="title" component={this.renderInput} label="Enter Title"/>
+                 <Field name="description" component={this.renderInput} label="Enter Description"/>
+             </form>
+         );
+     }
  }
 
- export default StreamCreate;
+ export default reduxForm({
+     form: 'streamCreate'
+ })(StreamCreate);
