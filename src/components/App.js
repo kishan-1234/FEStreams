b1f3a176b7a919aef9d78fae104e3094fdcbf361
kishan-1234 <kishan.ngm@gmail.com>
2020-11-08 00:09:51 +0530

    updated till form creation completion

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
-import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
-import Header from './header';
+import Header from './Header';
 
 const App = () => {
-    return <div className="ui ontainer">
-        <BrowserRouter>
+  return (
+    <div className="ui container">
+      <BrowserRouter>
         <div>
-        <Header/>
-            <Route path="/" exact component= {StreamList}/>
-            <Route path="/streams/new" exact component= {StreamCreate}/>
-            <Route path="/streams/edit" exact component= {StreamEdit}/>
-            <Route path="/streams/delete" exact component= {StreamDelete}/>
-            <Route path="/streams/show" exact component= {StreamShow}/>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
-        </BrowserRouter>
-    </div>;
-}
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App;  
+export default App;
--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import GoogleAuth from './GoogleAuth';
 
 const Header = () => {
-    return (
+  return (
     <div className="ui secondary pointing menu">
+      <Link to="/" className="item">
+        Streamy
+      </Link>
+      <div className="right menu">
         <Link to="/" className="item">
-            Streamer
+          All Streams
         </Link>
-        <div className="right menu">
-            <Link to="/" className="item">
-                All Streams
-            </Link>
-             <GoogleAuth/>
-        </div>
+        <GoogleAuth />
+      </div>
     </div>
-    );
-}
+  );
+};
 
 export default Header;
